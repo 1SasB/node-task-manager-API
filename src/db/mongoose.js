a25c8be964931a8b5b9f0646a028976f18056fe5
Benjamin Yeboah Sasu <sasuyeboahbenjamin@gmail.com>
2022-08-05 18:11:59 +0000

    added connection strong for mongo db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+mongoose.connect('mongodb+srv://1stilinski:psalm123@cluster0.ikxqn.mongodb.net/auth-project?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
